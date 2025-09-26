1781b372a067f1537c8ea4b29193e5b4c402c377
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2025-09-26 11:16:27 +0200

    updat/organize dirs v:2.1

--- scripts/organize_dirs_from_filenames.sh
+++ scripts/organize_dirs_from_filenames.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ###############################################################################
-# Script Name: organize_dirs_from_filenames.sh (v 1.2)
+# Script Name: organize_dirs_from_filenames.sh (v 2.1)
 # Author: Mustafa Demiroglu 
 # Description:
 #   This script organizes .tif files into specific folder structures based on
@@ -22,12 +22,12 @@
 #   - Files should be in the same directory as the script
 ###############################################################################
 
-# Loop through all the .tif files in the current directory
-for file in *.tif; do
+# Loop through all the image and document files with relevant extensions in the current directory
+for file in *.{tif,TIF,TIFF,jpg,jpeg,pdf}; do    
     # Extract parts of the filename using regular expressions
     # We assume the format is: <haus>_<bestand>_nr_<stück>_<number>.tif
-    if [[ "$file" =~ ^([a-zA-Z0-9]+)_([a-zA-Z0-9]+)_nr_([a-zA-Z0-9_-]+)_([0-9]+)\.tif$ ]]; then
-        haus="${BASH_REMATCH[1]}"     # Haus (e.g., hstam)
+    if [[ "$file" =~ ^([a-zA-Z0-9]+)_([a-zA-Z0-9]+)_nr_([a-zA-Z0-9_-]+)_([0-9]+)\.(tif|TIF|TIFF|jpg|jpeg|pdf)$ ]]; then
+    	haus="${BASH_REMATCH[1]}"     # Haus (e.g., hstam)
         bestand="${BASH_REMATCH[2]}"  # Bestand (e.g., karten)
         stueck="${BASH_REMATCH[3]}"   # Stück (e.g., c_261_b--2)
         number="${BASH_REMATCH[4]}"   # Number (e.g., 0002)
