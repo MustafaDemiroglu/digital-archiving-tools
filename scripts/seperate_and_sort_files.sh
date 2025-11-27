c75d40c7e2805a5d2c9f8a05ccd4b60a57d39901
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2025-11-27 21:31:01 +0100

    new script seperate and sort files fix3

--- scripts/seperate_and_sort_files.sh
+++ scripts/seperate_and_sort_files.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Check arguments
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <SOURCE_FOLDER> <TARGET_FOLDER> <START_NUMBER>"
+    exit 1
+fi
+
 SOURCE_FOLDER=$1
 TARGET_FOLDER=$2
 START_NUMBER=$3
@@ -13,7 +19,7 @@ if [ ! -d "$SOURCE_FOLDER" ]; then
     exit 1
 fi
 
-# Check target folder
+# Check / create target folder
 if [ -d "$TARGET_FOLDER" ]; then
     # Folder exists but is it empty?
     if [ "$(ls -A "$TARGET_FOLDER")" ]; then
@@ -37,13 +43,12 @@ FILES_TO_MOVE=$(echo "$FILES" | while read FILE; do
     fi
 done)
 
-COUNTER=$START_NUMBER
-
-# Move and rename all selected files
+# Move selected files and rename with increasing number starting 1
+COUNTER=1
 for FILE in $FILES_TO_MOVE; do
     NEW_FILE="${TARGET_FOLDER}/hhstaw_519--3_nr_${TARGET_FOLDER}_$(printf "%04d" $COUNTER).tif"
     mv "$FILE" "$NEW_FILE"
-    COUNTER=$(printf "%04d" $((COUNTER + 1)))
+    COUNTER=$((COUNTER + 1))
 done
 
 echo "File move and renaming completed successfully!"
