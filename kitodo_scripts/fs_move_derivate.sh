def222e8806389e678dedf60fb99f469f25c2b39
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2026-04-28 09:59:50 +0200

    rsync instead of mv and only max_images not more thumbs

--- kitodo_scripts/fs_move_derivate.sh
+++ kitodo_scripts/fs_move_derivate.sh
@@ -13,32 +13,13 @@ search_folder_vze
 final_kitodo_image_path="${kitodo_metadata_path}/${kitodo_processid}/images"
 final_path_new="/media/cepheus/derivate_on_demand/${meta_delivery}/secure/${full_sig_path}"
 
-echo "Creating folder structure in netapp folder: ${final_path_new}"
+echo "Creating folder structure: ${final_path_new}"
 sg "${group}" -c "mkdir -p ${final_path_new}/${kitodo_img_thumb_name}"
-echo "Moving derivate to final structure in NetApp"
-mv -n -v "${final_kitodo_image_path}/${kitodo_img_max_name}/"* "${final_path_new}"
-mv -n -v "${final_kitodo_image_path}/${kitodo_img_thumb_name}/"* "${final_path_new}/${kitodo_img_thumb_name}"
 
-if [ -d "${kitodo_metadata_path}/${kitodo_processid}/images/${kitodo_img_max_name}" ]; then
-    echo "Deleting folder ${kitodo_metadata_path}/${kitodo_processid}/images/${kitodo_img_max_name} to replace with symlinks"
-    if ! rmdir "${kitodo_metadata_path}/${kitodo_processid}/images/${kitodo_img_max_name}"; then
-        echo "folder not empty, so not deleting it! Aborting."
-        exit 1
-    fi  
-fi
+echo "Copying max images to final structure"
+rsync -av --ignore-existing "${final_kitodo_image_path}/${kitodo_img_max_name}/" "${final_path_new}/"
 
-if [ -d "${kitodo_metadata_path}/${kitodo_processid}/images/${kitodo_img_thumb_name}" ]; then
-    echo "Deleting folder ${kitodo_metadata_path}/${kitodo_processid}/images/${kitodo_img_thumb_name} to replace with symlinks"
-    if  ! rmdir "${kitodo_metadata_path}/${kitodo_processid}/images/${kitodo_img_thumb_name}"; then
-        echo "folder not empty, so not deleting it! Aborting."
-        exit 1
-    fi
-fi
-
-echo "Creating NetApp Symlink from ${final_path_new} to ${kitodo_metadata_path}/${kitodo_processid}/images/${kitodo_img_max_name}"
-ln -s "${final_path_new}" "${kitodo_metadata_path}/${kitodo_processid}/images/${kitodo_img_max_name}"
-echo "Creating NetApp Symlink from ${final_netapp_path}/${kitodo_img_thumb_name} ${kitodo_metadata_path}/${kitodo_processid}/images/${kitodo_img_thumb_name}"
-ln -s "${final_path_new}/${kitodo_img_thumb_name}" "${kitodo_metadata_path}/${kitodo_processid}/images/${kitodo_img_thumb_name}"
+echo "Max images copied successfully."
 	
 if ! find "${kitodo_metadata_path}/${kitodo_processid}/images/${kitodo_img_tiff_name}" -type f -delete; then
     echo "Error by deleting tiff working copy files! Aborting."
